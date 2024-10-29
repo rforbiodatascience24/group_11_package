51c5021bed62d04d661ead3dd17b1f21b9ea431a
elenitsep <eleni.tseperi@gmail.com>
2024-10-29 10:13:28 +0100

    Created data manual.

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Standard Codon Code
+#'
+#' Standard translation table from codons to amino acids
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_to_amino"

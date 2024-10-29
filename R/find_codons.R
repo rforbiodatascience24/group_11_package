df56f67fd9d60372cffc5c56491a20916141d5d9
Yoyoyohanne <johannemargrethelund@hotmail.dk>
2024-10-29 10:59:13 +0100

    find_codons function add

---
+++ R/find_codons.R
@@ -0,0 +1,16 @@
+#' Extract each individual codons from a sequence.
+#'
+#' @param sequence a RNA sequence
+#' @param start where in the sequence you want to start
+#'
+#' @return a string of codons
+#' @export
+#'
+#' @examples
+find_codons <- function(sequence, start = 1){
+  seq_length <- nchar(sequence)
+  codons <- substring(sequence,
+                      first = seq(from = start, to = seq_length-3+1, by = 3),
+                      last = seq(from = 3+start-1, to = seq_length, by = 3))
+  return(codons)
+}
